9663491f1c70cc2fc5934c3d91db81d0e9002850
sergio.canales.e <sergio.canales.e@aa1aa571-fc4e-6253-a26b-60f874dd404c>
2010-09-08 10:02:22 +0000

    Se implementa objetos escenario y Npc, comportamiento básico para cada uno. Se prueba el manejo de audio. Nota: La neuva clase Npc no hereda de Personaje como se planteo en un principio (replantear).

--- trunk/VideoJuego basico rpg/src/clases/Manager.java
+++ trunk/VideoJuego basico rpg/src/clases/Manager.java
@@ -13,6 +13,8 @@ import jgame.platform.JGEngine;
  */
 public class Manager extends JGEngine {
     public Jugador pj;
+    public Npc casa1;
+    public Npc casa1Npc;
     public StdDungeonMonster casa;
     public String msg;
     public static void main(String[] args) {
@@ -29,35 +31,45 @@ public class Manager extends JGEngine {
         initEngineApplet();
     }
 
+    @Override
     public void initCanvas() {
         // we set the background colour to same colour as the splash background
         setCanvasSettings(40, 30, 16, 16, JGColor.black, new JGColor(255, 246, 199), null);
 
     }
 
+    @Override
     public void initGame() {
         setFrameRate(60, 2);
-        defineMedia("/media/rpg.tbl");
-        defineImage("bgimage","-",0,"/media/imagenes/tiles/adoquines2.gif","-");
+
+        try{
+        defineMedia("/media/rpg-basico.tbl");
+        defineImage("bgimage","-",0,"/media/imagenes/tiles/Adoquin_16_base.png","-");
         setBGImage("bgimage");
+        playAudio("music", "musicaciudad",true);
+        }
+        catch(Exception ex){
+            System.out.println("Error al cargar medios: "+ex);
+        }
         //setMsgFont(new JGFont("Helvetica",0,32));
         setFont(new JGFont("Arial", 0, 20));
         setPFSize(80,60);//ventana de juego
         
-        pj = new Jugador(200,200 , 1);
-        casa = new CasaUno(-400,-200);
+        pj = new Jugador(400,400, 1);
+        casa1 = new Npc(100,100,"casa1","casa1",50,50);
 
         // create some tiles. "#" is our marble tile, "." is an empty space.
-        /*
+/*
         setTiles(
-                2, // tile x index
-                2, // tile y index
+                100, // tile x index
+                100, // tile y index
                 new String[]{"####", "#"} // A series of tiles. Each String represents a line of tiles.
                 );
         setTiles(13, 2, new String[]{"#####", "....#", "....#", "....#"});
         setTiles(13, 9, new String[]{"....#", "....#", "....#", "#####"});
 
-        setTiles(2, 9, new String[]{"#....", "#....", "#....", "#####"});
+        setTiles(2, 9, new String[]{"#....", "#....", "#....", "#####"});*/
+        /*
         // define the off-playfield tiles
         setTileSettings(
                 "#", // tile that is found out of the playfield bounds
@@ -67,24 +79,30 @@ public class Manager extends JGEngine {
 
 
     }
-
+/*
     public class CasaUno extends StdDungeonMonster {
             public CasaUno(double x,double y) {
                     super("casa",true,x,y,4,"explo",685);
                     if (isMidlet()) this.expiry = suspend_off_view;
             }
     }
-
+*/
     /** View offset. */
     int xofs=0,yofs=0;
 
     @Override
     public void doFrame() {
 
+        if((pj.isAusente())&&(getMouseButton(1))&&(getMouseY()>200)){
+            pj = new Jugador(180,210 , 1);
+            removeObjects("casa1Npc", 51);
+        }else if(pj.isAusente()){
+            casa1 = new Npc(300,100,"casa1Npc","casa1Npc",51,51);
+        }
         moveObjects(null, 0);
         // check object-object collision
         checkCollision(
-                1, // cids of objects that our objects should collide with
+                50, // cids of objects that our objects should collide with
                 1 // cids of the objects whose hit() should be called
                 );
         // check object-tile collision
@@ -101,12 +119,6 @@ public class Manager extends JGEngine {
                 xofs,yofs,
                 true   
          );
-
     }
-
-    @Override
-    public void paintFrame() {}
-
-    
 }
 
---
+++ trunk/VideoJuego basico rpg/src/clases/Npc.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package clases;
+
+import extensiones.StdDungeonMonster;
+/**
+ *
+ * @author Iarwain
+ */
+public class Npc extends StdDungeonMonster  {
+
+    public Npc(double x,double y,String name,String mediaName,int colId,int tamano) {
+            super(name,true,x,y,colId,mediaName,tamano);
+
+         
+    }
+
+
+
+
+    
+}
