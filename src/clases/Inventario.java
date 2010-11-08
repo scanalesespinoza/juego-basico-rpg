a958b161c3e67184a5d8da163500a287161095ea
ger.schism <ger.schism@aa1aa571-fc4e-6253-a26b-60f874dd404c>
2010-11-08 02:20:15 +0000

    Métodos para salvar en la base de datos implementados en las clases: 	* Inventario 	*Contrincantehabilidad Falta encargo | hay una complicación con el manejo de la clave primary en el hashmap ..revisar
    En la clase Habilidad se implemento el método "setHabilidad(habilidad_id) que setea el objeto,según el id entregado como parámetro, desde la base de datos.

--- src/clases/Inventario.java
+++ src/clases/Inventario.java
@@ -46,6 +46,7 @@ public class Inventario {
                 item.setIdPersonaje(res.getShort("idPersonaje"));
                 item.setCantidad(res.getShort("cantidad"));
                 item.setEstaEquipado(res.getShort("estaequipado"));
+                item.setNewItem(false);
                 this.objetos.put(item.getIdObjeto(), item);
                 i += 1;
 
@@ -55,56 +56,174 @@ public class Inventario {
         }
 
     }
-    /*
-     * agrega un item al inventario, si el item está, se suma la cantidad
-     * 
-     */
 
+    /************************IMPORTANTE*****************************************/
+    //    Para manipular correctamente la base de datos, se ha diseñado una serie de
+    //    convenciones:
+    //        *Al adherir un item se comprueba si ya está en el hashmap, si no esta
+    //        se agrega al hashmap y se pone el valor newItem como true
+    //        esto hace que, independiente de lo que sucede con sus datos en el
+    //        transcurso  del juego , se guarde en la base de datos
+    //
+    //        *Para borrar un item , independiente si está o no en la base de datos
+    //        se debe dejar la cantidad en 0, asi cada vez que se guarde, se eliminaran
+    //        en la base de datos los elementos correspondientes, y se volverá a cargar
+    //        el hashmap
+    /***************************************************************************/
+    /**
+     * elimina, inserta y actualiza en la base de datos según los datos que tenga
+     * en el hashmap
+     */
+    public void salvarInventario() {
+        bdDeletes();
+        bdUpdates();
+        bdInserts();
+    }
+
+    /**
+     * Busca los elementos que ya están en la base de datos (newItem == true)
+     * y que no hayan sido eliminados de alguna manera (cantidad > 0 )
+     * para hacer el respectivo update en la base de datos
+     */
+    private void bdUpdates() {
+        this.conexion = new dbDelegate();
+        Iterator it = this.getObjetos().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            Item item = this.getItem(Short.parseShort(e.getKey().toString()));
+            if (!item.isNewItem() && item.getCantidad() > 0) {
+                String StrSql = "UPDATE inventario"
+                        + " SET cantidad = " + item.getCantidad() + ","
+                        + "     estaEquipado = " + item.getEstaEquipado()
+                        + " WHERE personaje_id = " + this.getIdPersonaje()
+                        + "   AND objeto_id = " + item.getIdObjeto();
+                conexion.Ejecutar(StrSql);
+            }
+        }
+    }
+    /**
+     * Si el item es nuevo y no ha sido eliminado (cantidad == 0)
+     * insertar en la base de datos
+     */
+    private void bdInserts() {
+        this.conexion = new dbDelegate();
+        Iterator it = this.getObjetos().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            Item item = this.getItem(Short.parseShort(e.getKey().toString()));
+            if (item.isNewItem() && item.getCantidad() > 0) {
+                String StrSql = "INSERT INTO inventario VALUES("
+                        + this.getIdPersonaje() + ","
+                        + item.getIdObjeto() + ","
+                        + item.getCantidad() + ","
+                        + item.getEstaEquipado() + ")";
+                conexion.Ejecutar(StrSql);
+            }
+        }
+    }
+    /**
+     * si el item no tiene cantidad , se borra de la base de datos
+     */
+    private void bdDeletes() {
+        this.conexion = new dbDelegate();
+        Iterator it = this.getObjetos().entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            Item item = this.getItem(Short.parseShort(e.getKey().toString()));
+            if (item.getCantidad() <= 0) {
+                String StrSql = "DELETE FROM inventario WHERE"
+                        + " personaje_id = " + this.getIdPersonaje()
+                        + " AND objeto_id = " + item.getIdObjeto();
+                conexion.Ejecutar(StrSql);
+            }
+        }
+    }
+
+    private Item getItem(short idItem) {
+        return this.getObjetos().get(idItem);
+    }
+
+    /**
+     * Agrega un item al inventario, si el item está, se suma la cantidad
+     * @param idItem
+     * @param cantidad
+     */
     public void agregarItem(short idItem, short cantidad) {
         Item item = creaItem(idItem, cantidad, (short) 0);
-        if (this.tieneItem(idItem)) {
+        if (this.comprobarItem(idItem)) {
             this.getObjetos().get(idItem).sumarCantidad(cantidad);
         } else {
+            //si el item es nuevo en el hashmap, se debe ingresar en la base de datos
+            //para eso se pone el valor newItem como true
+            item.setNewItem(true);
             this.getObjetos().put(idItem, item);
         }
     }
 
+    /**
+     *  Agrega UN item al inventario
+     * @param idItem
+     */
     public void agregarItem(short idItem) {
         Item item = creaItem(idItem, (short) 1, (short) 0);
-        if (this.tieneItem(idItem)) {
+        if (this.comprobarItem(idItem)) {
             this.getObjetos().get(idItem).sumarCantidad((short) 1);
         } else {
+            //si el item es nuevo en el hashmap, se debe ingresar en la base de datos
+            //para eso  se pone el valor newItem como true
+            item.setNewItem(true);
             this.getObjetos().put(idItem, item);
         }
     }
-    /*
-     * Borra item según la cantidad indicada
-     */
 
+    /**
+     * Borra item según la cantidad indicada
+     * @param idItem
+     * @param cantidad
+     */
     public void eliminarItem(short idItem, short cantidad) {
         if (tieneItem(idItem, cantidad)) {
             this.getObjetos().get(idItem).restarCantidad(cantidad);
         }
     }
-    /*
-     * Borra item sin importar la cantidad
-     */
 
+    /**
+     * Borra item sin importar la cantidad
+     * @param idItem
+     */
     public void eliminarItem(short idItem) {
-        if (tieneItem(idItem)) {
-            this.getObjetos().remove(idItem);
+        if (comprobarItem(idItem)) {
+            this.getObjetos().get(idItem).setCantidad((short) 0);
         }
     }
 
-    /*
-     * Verifica si tiene el objeto en cuestión
+    /**
+     * Verifica si tiene el objeto en cuestión en el hashmap, lógicamente
+     * para tener un item se debe tener una cantidad mínima de 1
+     * Este método es para fines de implementación y solo contempla
+     * si existe en el hashmap (pudiendo tener cantidad o)
+     * @param idObjeto
+     * @return
      */
-    public boolean tieneItem(short idObjeto) {
+    private boolean comprobarItem(short idObjeto) {
         return this.getObjetos().containsKey(idObjeto);
     }
 
-    /*
+    /**
+     * Verifica si tiene almenos un item en cuestion, este método es para
+     * saber si lógicamente tiene el item (almenos una unidad)
+     * @param idItem
+     * @return
+     */
+    public boolean tieneItem(short idItem) {
+        return tieneItem(idItem, (short) 1);
+    }
+
+    /**
      * Verifica si tiene la cantidad requerida del objeto en cuestión
+     * @param idObjeto
+     * @param cantidad
+     * @return
      */
     public boolean tieneItem(short idObjeto, short cantidad) {
         return this.getObjetos().containsKey(idObjeto)
@@ -112,24 +231,25 @@ public class Inventario {
 
     }
 
-    /*
+    /**
      * retorna el el peso total usado por el inventario(suma de los objetos)
+     * @return
      */
     public int getPesoUsado() {
-        //objeto la lista de objetos
         int pesoTotal = 0;
         Iterator it = this.getObjetos().entrySet().iterator();
         Objeto obj = new Objeto();
         while (it.hasNext()) {
             Map.Entry e = (Map.Entry) it.next();
             obj.setObjeto(Short.parseShort(e.getKey().toString()));
-            pesoTotal += obj.getPeso();
+            pesoTotal += obj.getPeso() * this.getObjetos().get(Short.parseShort(e.getKey().toString())).getCantidad();
         }
         return pesoTotal;
     }
 
-    /*
-     * retorna el valor en dinero que suman los elementos
+    /**
+     * Retorna el valor en dinero que suman los elementos
+     * @return
      */
     public int getDineroTotal() {
         //objeto la lista de objetos
@@ -139,13 +259,17 @@ public class Inventario {
         while (it.hasNext()) {
             Map.Entry e = (Map.Entry) it.next();
             obj.setObjeto(Short.parseShort(e.getKey().toString()));
-            dineroTotal += obj.getValorDinero();
+            dineroTotal += obj.getValorDinero() * this.getObjetos().get(Short.parseShort(e.getKey().toString())).getCantidad();
         }
         return dineroTotal;
     }
 
-    /*
+    /**
      * Crea un item privado para centralizar la creacion y favorecer la modularidad
+     * @param idObjeto
+     * @param cantidad
+     * @param estaequipado
+     * @return
      */
     private Item creaItem(short idObjeto, short cantidad, short estaequipado) {
         return new Item(this.getIdPersonaje(), idObjeto, cantidad, estaequipado);
@@ -168,17 +292,18 @@ public class Inventario {
     /*
      * Devuelve la cantidad de objetos de un determinado tipo
      */
-    public int contarItem(short idItem){
+    public int contarItem(short idItem) {
         int cuenta = 0;
-        if (tieneItem(idItem)){
+        if (tieneItem(idItem)) {
             cuenta = this.getObjetos().get(idItem).getCantidad();
         }
         return cuenta;
     }
-    /*
-     * devuele la cantidad en total de todos los objetos
-     */
 
+    /**
+     * Devuele la cantidad en total de todos los objetos
+     * @return
+     */
     public int contarTodosItems() {
         //objeto la lista de objetos
         int cuenta = 0;
@@ -208,6 +333,8 @@ public class Inventario {
         private short idObjeto;
         private short cantidad;
         private short estaEquipado;
+        private boolean newItem;
+        private boolean eliminarItem;
 
         public Item() {
         }
@@ -217,6 +344,22 @@ public class Inventario {
             this.idObjeto = idObjeto;
             this.cantidad = cantidad;
             this.estaEquipado = estaEquipado;
+        }
+
+        public boolean isEliminarItem() {
+            return eliminarItem;
+        }
+
+        public void setEliminarItem(boolean eliminarItem) {
+            this.eliminarItem = eliminarItem;
+        }
+
+        public boolean isNewItem() {
+            return newItem;
+        }
+
+        public void setNewItem(boolean newItem) {
+            this.newItem = newItem;
         }
 
         public short getCantidad() {
@@ -254,11 +397,12 @@ public class Inventario {
         public void sumarCantidad(short cantidad) {
             this.setCantidad((short) (this.getCantidad() + cantidad));
         }
-        /*
-         * borra la cantidad indicada, si es menor que cero lo deja en cero
-         *
-         */
 
+        /**
+         * Borra la cantidad indicada, si es menor que cero lo deja en cero
+         *
+         * @param cantidad
+         */
         public void restarCantidad(short cantidad) {
             short valor = (short) (this.getCantidad() - cantidad);
             if (this.getCantidad() - cantidad < 0) {
